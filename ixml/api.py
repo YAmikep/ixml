e6fb3655548a6de4ad74baeb18dae434f4b5faba
Michael Palumbo <michael.palumbo87@gmail.com>
2013-08-11 18:43:19 +0200

    Created a temporary fake fallback backend to avoid errors while pip installing if lxml is not installed.

--- ixml/api.py
+++ ixml/api.py
@@ -7,9 +7,14 @@ try:
     # Known as the fastest XML library in python
     import ixml.backends.lxmliterparse as _backend
 except ImportError:
-    pass
-    #raise Exception(
-    #    'There is currently only one backend implemented so you must install lxml to use it.')
+    def _backend(Object):
+        def parse(self, *args, **kwargs):
+            raise Exception(
+                'There is currently only a lxml backend so you must install lxml to use it.')
+        def items(self, *args, **kwargs):
+            raise Exception(
+                'There is currently only a lxml backend so you must install lxml to use it.')
+
     # TODO: direct binding to the C libraries to avoid unused intermediate Element objects?
     # TODO: Fallback to some other backends: standard library ElementTree,
     # etc.?
